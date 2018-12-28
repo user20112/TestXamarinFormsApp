7e45885bb993f58582eb8f123c707eb4944c72ad
user20112 <devlinpaddock@gmail.com>
2018-12-27 17:53:40 -0800

    Commited right folder.
    I had commited the wrong folder last time and had only included one project instead of the whole solution.. sorry

---
+++ TestXamarinFormsApp/TestXamarinFormsApp/ImageModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TestXamarinFormsApp
+{
+    class ImageModel
+    {
+        public string Title { get; set; }
+        public Image ActualImage { get; set; }
+        public Image Imperfections { get; set; }
+        public string Description { get; set; }
+
+
+        public ImageModel(string title, ImageSource actualImage, ImageSource imperfections, string description)
+        {
+            Title = title;
+            ActualImage = new Image();
+            Imperfections = new Image();
+            ActualImage.Source = actualImage;
+            Imperfections.Source = imperfections;
+            Description = description;
+        }
+    }
+
+}
